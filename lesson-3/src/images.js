f05062f2d50d3f8756de3f25b9bf8b151fe07998
Your Name <you@example.com>
2022-10-28 21:00:46 +0700

    btvn3

--- lesson-3/src/images.js
+++ lesson-3/src/images.js
@@ -1,16 +1,16 @@
 const images = [{
     id: 1,
-    src: "lesson-3/src/assets/img/img1.jpg",
+    src: "'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'",
     alt: "Image 1"
 },
 {
     id: 2,
-    src: "lesson-3/src/assets/img/img2.jpg",
+    src: "https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U",
     alt: "Image 2 "
 },
 {
     id: 3,
-    src: "lesson-3/src/assets/img/img3.jpg",
+    src: "https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U",
     alt: "Image 3"
 }
 ];
--- lesson-3/src/index.js
+++ lesson-3/src/index.js
@@ -2,21 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Carousel from 'react-bootstrap/Carousel'
-const root = ReactDOM.createRoot(document.getElementById("root"))
-const ListImage =  [
-    'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-    'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-    'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-    'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',
-    'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'
-]
-root.render
-(<React.StrictMode>
-<Carousel>
-    {ListImage.map(value, index)} => <CarouselItem image = {value}></CarouselItem>
-<CarouselItem />
-
-</Carousel>
-
-</React.StrictMode>);
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
