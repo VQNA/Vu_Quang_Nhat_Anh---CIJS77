f5bf06cde281a7504123ad519aea1d5fe93224d8
Your Name <you@example.com>
2022-12-10 23:40:04 +0700

    commit

--- finals/src/components/Active/index.js
+++ finals/src/components/Active/index.js
@@ -1,106 +1,106 @@
-    // Todo.js
-    
-    import React, { useState, useEffect } from 'react';
-    import "./active.css";
+// Todo.js
 
-    
-    function Active() {
-        const [newItem, setNewItem] = useState("");
-        const [items, setItems] = useState([]);
-      
-        function addItem() {
-          if (!newItem) {
+import React, { useState, useEffect } from 'react';
+import "./active.css";
+
+
+function Active() {
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
+
+    function addItem() {
+        if (!newItem) {
             alert("Press enter an item.");
             return;
-          }
-      
-          const item = {
+        }
+
+        const item = {
             id: Math.floor(Math.random() * 1000),
             value: newItem,
             status: true
-          };
-      
-          setItems((oldList) => [...oldList, item]);
-      
-          setNewItem("");
-        }
-      
-        function deleteItem(id) {
-          const newArray = items.filter((item) => item.id !== id);
-          setItems(newArray);
-        }
-      
-        function changeState(id){
-          let ChangeTask = items.map( task => {
-              if( task.id === id ) {
-                return ({ ...task, status: !task.status })
-              }
-              return task;
-            })
-            setItems(ChangeTask);
-          }
-        
-      
-        function deleteAll() {
-          const newArray = []
-          setItems(newArray);
-        }
-      
-        useEffect(() => {
-          const LSitems = JSON.parse(localStorage.getItem('items'));
-          if (LSitems) {
-           setItems(LSitems);
-          }
-        }, []);
-        useEffect(() => {
-            localStorage.setItem('items', JSON.stringify(items));
-          }, [items]);
-        
+        };
 
-        
-      
-        
-        return (
-          <div className="app">
-      
+        setItems((oldList) => [...oldList, item]);
+
+        setNewItem("");
+    }
+
+    function deleteItem(id) {
+        const newArray = items.filter((item) => item.id !== id);
+        setItems(newArray);
+    }
+
+    function changeState(id) {
+        let ChangeTask = items.map(task => {
+            if (task.id === id) {
+                return ({ ...task, status: !task.status })
+            }
+            return task;
+        })
+        setItems(ChangeTask);
+    }
+
+
+    function deleteAll() {
+        const newArray = []
+        setItems(newArray);
+    }
+
+    useEffect(() => {
+        const LSitems = JSON.parse(localStorage.getItem('items'));
+        if (LSitems) {
+            setItems(LSitems);
+        }
+    }, []);
+    useEffect(() => {
+        localStorage.setItem('items', JSON.stringify(items));
+    }, [items]);
+
+
+
+
+
+    return (
+        <div className="app">
+
             <input
-              type="text"
-              placeholder="Add task..."
-              value={newItem}
-              onChange={(e) => setNewItem(e.target.value)}
+                type="text"
+                placeholder="Add task..."
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
             />
-      
+
             <button onClick={() => addItem()}>Add</button>
-            
+
             <ul>
-              {items.map((item) => {
-                return (
-                
-                  <div className={item.status ? 'ongoingA' : 'doneA'}>
-                    <li key={item.id}>
-                      {item.value}
-                      <button
-                        className="delete-button"
-                        onClick={() => deleteItem(item.id)}
-                      >
-                        Delete
-                      </button>
-                      <button
-                        className="update-button"
-                        onClick={() => changeState(item.id)}
-                      >
-                        Update
-                      </button>
-                    </li>
-      
-                    
-                  </div>
-                );
-              })}
+                {items.map((item) => {
+                    return (
+
+                        <div className={item.status ? 'ongoingA' : 'doneA'}>
+                            <li key={item.id}>
+                                {item.value}
+                                <button
+                                    className="delete-button"
+                                    onClick={() => deleteItem(item.id)}
+                                >
+                                    Delete
+                                </button>
+                                <button
+                                    className="update-button"
+                                    onClick={() => changeState(item.id)}
+                                >
+                                    {item.status ? 'Finish' : 'Unfinish'}
+                                </button>
+                            </li>
+
+
+                        </div>
+                    );
+                })}
             </ul>
-            {items.length > 1 ? <button onClick = {deleteAll}>Delete All</button> : ""}
-          </div>
-        );
-      }
-    
-    export default Active;
+            {items.length > 1 ? <button onClick={deleteAll}>Delete All</button> : ""}
+        </div>
+    );
+}
+
+export default Active;
--- finals/src/components/Todo/index.js
+++ finals/src/components/Todo/index.js
@@ -2,100 +2,100 @@ import React, { useState, useEffect } from "react";
 import "./todo.css";
 
 function ToDo() {
-  const [newItem, setNewItem] = useState("");
-  const [items, setItems] = useState([]);
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
 
-  function addItem() {
-    if (!newItem) {
-      alert("Press enter an item.");
-      return;
-    }
-
-    const item = {
-      id: Math.floor(Math.random() * 1000),
-      value: newItem,
-      status: true
-    };
-
-    setItems((oldList) => [...oldList, item]);
-
-    setNewItem("");
-  }
-
-  function deleteItem(id) {
-    const newArray = items.filter((item) => item.id !== id);
-    setItems(newArray);
-  }
-
-  function changeState(id){
-    let ChangeTask = items.map( task => {
-        if( task.id === id ) {
-          return ({ ...task, status: !task.status })
+    function addItem() {
+        if (!newItem) {
+            alert("Press enter an item.");
+            return;
         }
-        return task;
-      })
-      setItems(ChangeTask);
+
+        const item = {
+            id: Math.floor(Math.random() * 1000),
+            value: newItem,
+            status: true
+        };
+
+        setItems((oldList) => [...oldList, item]);
+
+        setNewItem("");
     }
-  
 
-  function deleteAll() {
-    const newArray = []
-    setItems(newArray);
-  }
-
-  useEffect(() => {
-    const LSitems = JSON.parse(localStorage.getItem('items'));
-    if (LSitems) {
-     setItems(LSitems);
+    function deleteItem(id) {
+        const newArray = items.filter((item) => item.id !== id);
+        setItems(newArray);
     }
-  }, []);
 
-  useEffect(() => {
-    localStorage.setItem('items', JSON.stringify(items));
-  }, [items]);
+    function changeState(id) {
+        let ChangeTask = items.map(task => {
+            if (task.id === id) {
+                return ({ ...task, status: !task.status })
+            }
+            return task;
+        })
+        setItems(ChangeTask);
+    }
 
-  
 
-  
-  return (
-    <div className="app">
+    function deleteAll() {
+        const newArray = []
+        setItems(newArray);
+    }
 
-      <input
-        type="text"
-        placeholder="Add task..."
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-      />
+    useEffect(() => {
+        const LSitems = JSON.parse(localStorage.getItem('items'));
+        if (LSitems) {
+            setItems(LSitems);
+        }
+    }, []);
 
-      <button onClick={() => addItem()}>Add</button>
+    useEffect(() => {
+        localStorage.setItem('items', JSON.stringify(items));
+    }, [items]);
 
-      <ul>
-        {items.map((item) => {
-          return (
-            <div className={item.status ? 'ongoing' : 'done'}>
-              <li key={item.id}>
-                {item.value}
-                <button
-                  className="delete-button"
-                  onClick={() => deleteItem(item.id)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="update-button"
-                  onClick={() => changeState(item.id)}
-                >
-                  Update
-                </button>
-              </li>
 
-              
-            </div>
-          );
-        })}
-      </ul>
-      {items.length > 1 ? <button onClick = {deleteAll}>Delete All</button> : ""}
-    </div>
-  );
+
+
+    return (
+        <div className="app">
+
+            <input
+                type="text"
+                placeholder="Add task..."
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+            />
+
+            <button onClick={() => addItem()}>Add</button>
+
+            <ul>
+                {items.map((item) => {
+                    return (
+                        <div className={item.status ? 'ongoing' : 'done'}>
+                            <li key={item.id}>
+                                {item.value}
+                                <button
+                                    className="delete-button"
+                                    onClick={() => deleteItem(item.id)}
+                                >
+                                    Delete
+                                </button>
+                                <button
+                                    className="update-button"
+                                    onClick={() => changeState(item.id)}
+                                >
+                                    {item.status ? 'Finish' : 'Unfinish'}
+                                </button>
+                            </li>
+
+
+                        </div>
+                    );
+                })}
+            </ul>
+            {items.length > 1 ? <button onClick={deleteAll}>Delete All</button> : ""}
+        </div>
+    );
 }
 export default ToDo
